323b2a1ab800d78351dd60ec6e5df137a268d18f
Benjycheng <benjamin.cheng.2003@gmail.com>
2025-03-03 14:20:49 +1100

    Delete Verilog directory

--- Verilog/Modules/DeMUX4.v
+++
@@ -1,6 +0,0 @@
-module DeMUX4(input [1:0] sel, input in, output reg [3:0] out);	
-	always @(*) begin
-	out = 4'b0000;
-	out[sel] = in;
-	end
-endmodule
--- Verilog/Modules/Toggle.v
+++
@@ -1,4 +0,0 @@
-module Toggle (input clk, in, output reg out);
-	always @(posedge in)
-		if (in) out <= !out;
-endmodule
